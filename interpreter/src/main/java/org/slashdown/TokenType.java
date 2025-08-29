20b7b680534cf66c1d20d5c395df7cbbb22b3de9
Kristoffer Paulsson <kristoffer.paulsson@talenten.se>
2025-08-29 17:00:54 +0200

    Adding

--- interpreter/src/main/java/org/slashdown/TokenType.java
+++ interpreter/src/main/java/org/slashdown/TokenType.java
@@ -3,5 +3,6 @@ package org.slashdown;
 public enum TokenType {
     WHITESPACE,
     NON_WHITESPACE,
-    SYMBOL;
+    SYMBOL,
+    WORD;
 }
---
+++ interpreter/src/main/java/org/slashdown/TokenWord.java
@@ -0,0 +1,43 @@
+/**
+ * Copyright (c) 2023-2025 by Kristoffer Paulsson <kristoffer.paulsson@talenten.se>.
+ * <p>
+ * Permission to use, copy, modify, and/or distribute this software for any purpose with
+ * or without fee is hereby granted, provided that the above copyright notice and this
+ * permission notice appear in all copies.
+ * <p>
+ * THE SOFTWARE IS PROVIDED “AS IS” AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH REGARD
+ * TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN
+ * NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR
+ * CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR
+ * PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION,
+ * ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ * <p>
+ * https://opensource.org/licenses/ISC
+ * <p>
+ * SPDX-License-Identifier: ISC
+ * <p>
+ * Contributors:
+ * Kristoffer Paulsson - initial implementation
+ */
+package org.slashdown;
+
+import java.util.Set;
+
+public class TokenWord extends TokenScanner {
+    static public final Set<Character> CHARACTERS = Set.of(
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+            'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+            'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+            '_',
+            '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'
+    );
+
+    public boolean isValid(char c) {
+        return CHARACTERS.contains(c);
+    }
+
+    public TokenType getType() {
+        return TokenType.SYMBOL;
+    }
+}
